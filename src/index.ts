aec24a4167bfcf2de6ca9eab15a4be1e3346d114
Cell6969 <bossmarinoo@gmail.com>
2024-01-27 20:48:06 +0700

    Initialize Project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({
+        data: "hello World"
+    })
+})
+
+app.listen(port, () => console.log('listening on port 4000'));
